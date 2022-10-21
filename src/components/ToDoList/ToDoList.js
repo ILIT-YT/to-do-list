c7a5a44c6aa7e91d6d3c2d5e742cfeefc3d318a6
ILIT-YT <116321196+ILIT-YT@users.noreply.github.com>
2022-10-20 20:39:27 -0400

    Add files via upload

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,35 +1,71 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import ListItem from '../ListItem/ListItem';
 
+function itemListReducer(state, action){
+    switch (action.type){
+        case "ADD_ITEM":
+            
+            return {
+                item: "",
+                itemList: [...state.itemList, {item: state.item, id: state.itemsAdded+1}],
+                itemsAdded: state.itemsAdded + 1
+            }
+        case "CHANGE_ITEM":
+            return {
+                item: action.item,
+                itemList: state.itemList,
+                itemsAdded: state.itemsAdded
+            }
+        case "DELETE_ITEM":
+            return {
+                item: state.item,
+                itemList: state.itemList.filter(item => action.id !== item.id),
+                itemsAdded: state.itemsAdded
+            }
+    }
+}
+
+const getInitItemState = () => {
+    return {
+        item: 'wash dishes',
+        itemList: [],
+        itemsAdded: 0
+    }
+}
+
 const ToDoList = () => {
-    const [item, setItem] = useState('do homework');
-    const [itemList, setItemList] = useState([]);
-    const [itemsAdded, setItemsAdded] = useState(0);
+    const [state, dispatch] = useReducer(itemListReducer,{}, getInitItemState)
+    // const [item, setItem] = useState('do homework');
+    // const [itemList, setItemList] = useState([]);
+    // const [itemsAdded, setItemsAdded] = useState(0);
 
     const onChangeInput = (event) => {
-        setItem(event.target.value);
+        // setItem(event.target.value);
+        dispatch({type:"CHANGE_ITEM", item: event.target.value})
     }
 
     const submitHandler = (event) => {
         event.preventDefault();
-        setItemsAdded(itemsAdded + 1);
-        setItemList([...itemList, {
-            item: item,
-            id: itemsAdded
-        }]);
-        setItem('');
+        // setItemsAdded(itemsAdded + 1);
+        // setItemList([...itemList, {
+        //     item: item,
+        //     id: itemsAdded
+        // }]);
+        // setItem('');
+        dispatch({type:"ADD_ITEM"})
     }
 
     const deleteHandler = (id) => {
-        setItemList(itemList.filter(item => id !== item.id));
+        // setItemList(itemList.filter(item => id !== item.id));
+        dispatch({type:"DELETE_ITEM", id: id})
     }
 
-    const getListItems = itemList.map(item => <ListItem item={item.item} delete={deleteHandler} id={item.id} key={item.id} />)
+    const getListItems = state.itemList.map(item => <ListItem item={item.item} delete={deleteHandler} id={item.id} key={item.id} />)
 
   return (
     <div>
         <form onSubmit={submitHandler}>
-            <input type="text" value={item} onChange={onChangeInput}/>
+            <input type="text" value={state.item} onChange={onChangeInput}/>
             <button type='submit' >Submit</button>
         </form>
         {getListItems}
